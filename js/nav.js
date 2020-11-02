108c2705f8c338dfff625005e3611bb2658e7d72
Catherine Grillo <cgrillo@umich.edu>
2020-11-02 12:45:55 -0500

    added more credits to photos and changed menu styling and text decoration

--- js/nav.js
+++ js/nav.js
@@ -1,8 +1,8 @@
 // This function is modeled after the one used in the W3Schools example
 // Link: https://www.w3schools.com/howto/howto_js_mobile_navbar.asp
 
-function myFunction() {
-    var x = document.getElementById("links");
+function openMenu() {
+    var x = document.getElementById("pages");
     if (x.style.display === "block") {
       x.style.display = "none";
     } else {
